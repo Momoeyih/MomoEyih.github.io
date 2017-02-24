74f10a9af1b6e8bfa98554eb30d3aa15cfcca6be
Momoeyih <momo_eyih@hotmail.fr>
2017-02-24 15:57:16 +0100

    TD 1 - PART 3

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,14 @@
+
+
+
+
+
+
+function homePageLoading() {
+     = 'apiUrl';
+    elementID = 'idDeVotreElementHtml';
+    fonctionRequeteApi(url, elementID);
+
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -8,4 +19,5 @@ xmlhttp.onreadystatechange = function() {
 };
 xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
 xmlhttp.send();
-		
+    
+}
